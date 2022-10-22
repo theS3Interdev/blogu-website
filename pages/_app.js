9e2762f4f38c9cf6208d461952630d3e80a4d1ae
Michael Owuor <michael.owuor@gmail.com>
2022-10-22 19:48:28 +0300

    feat: display blog posts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import client from '../lib/apollo-client';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<ApolloProvider client={client}>
+			<Component {...pageProps} />;
+		</ApolloProvider>
+	);
 }
 
 export default MyApp;

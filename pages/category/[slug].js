7c9f049161c6f7058319e93d84e11e2e2af9d3ab
Michael Owuor <michael.owuor@gmail.com>
2022-10-25 11:08:12 +0300

    fix: update project dependencies (#15)

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -34,7 +34,7 @@ const CatgegoryPost = ({ posts }) => {
 	}
 
 	return (
-		<Layout title="Categories">
+		<Layout title="Category">
 			<div className="container mx-auto mb-8 px-10">
 				<div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
 					<div className="col-span-1 lg:col-span-8">

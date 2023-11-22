bdf4adcba418706dc2360e9ed8fdebcd1a0d2e17
Michael Owuor <michael.owuor@gmail.com>
2023-11-22 10:00:04 +0300

    chore: update project dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		fontFamily: {
-			ubuntu: 'Ubuntu, sans-serif',
-		},
-		extend: {
-			backgroundImage: {
-				base: 'url("/bg.jpg")',
-			},
-		},
-	},
-	plugins: [
-		require('@tailwindcss/aspect-ratio'),
-		require('@tailwindcss/forms'),
-		require('@tailwindcss/line-clamp'),
-		require('@tailwindcss/typography'),
-	],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      ubuntu: "Ubuntu, sans-serif",
+    },
+    extend: {
+      backgroundImage: {
+        base: 'url("/bg.jpg")',
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
 };
